57e01ddb34063a6ee3e13f0da0ef3c267fe0f93d
phyex0 <halit_burak@outlook.com>
2021-12-05 11:56:14 +0300

    Test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,12 @@
 package com.company;
 
 
-
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
         int[] arr = new int[]{1, 32, 4, 55, 0, 7, 4, 3, 2, 45, 6, 32, 3, 43, 431, 43, 14, 13, 413, 99, 413, 4, 13, 42, 42, 45, 42, 24, 1, 245, 11, 3, 7, 875, 9, 6, 85};
-
+        System.out.println("Test");
     }
 
 
